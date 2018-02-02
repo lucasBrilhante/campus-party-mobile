699d1c5187f110bbbb083a32625e1d237e4f0f72
Lucas Brilhante <lucasb@uab.edu>
2018-02-02 03:27:47 -0200

    Getting json from api

--- app/src/main/java/view/fragments/AgendaListFragment.java
+++ app/src/main/java/view/fragments/AgendaListFragment.java
@@ -4,9 +4,21 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +31,7 @@ import view.HeaderRecyclerViewSection;
 public class AgendaListFragment extends Fragment {
 
     private List<Activity> activities;
-
+    SectionedRecyclerViewAdapter sectionAdapter;
     public AgendaListFragment() {
 
     }
@@ -32,13 +44,17 @@ public class AgendaListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_agenda, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_agenda, container, false);
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+        String url ="https://sandboxapi.campuse.ro/agenda/list/campus-party-brasil-2018/";
 
         RecyclerView sectionHeader = (RecyclerView) rootView.findViewById(R.id.add_header);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
         sectionHeader.setLayoutManager(linearLayoutManager);
         sectionHeader.setHasFixedSize(true);
-        SectionedRecyclerViewAdapter sectionAdapter = new SectionedRecyclerViewAdapter();
+        sectionAdapter = new SectionedRecyclerViewAdapter();
 
         List<String> hours = new ArrayList<>();
         hours.add("10:00");
@@ -51,6 +67,41 @@ public class AgendaListFragment extends Fragment {
         }
 
         sectionHeader.setAdapter(sectionAdapter);
+
+        // Request a string response from the provided URL.
+        JsonObjectRequest activityRequest = new JsonObjectRequest(Request.Method.GET, url,null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Display the first 500 characters of the response string.
+                        Log.d("DEU BOOOOOOOOOOOOOOOOM ",response.toString());
+
+
+                        try {
+                            int count = response.getInt("count");
+
+
+                            for(int i = 0;i<count;i++) {
+                                JSONObject activityJson = response.getJSONArray("results").getJSONObject(i);
+                                Activity activity = new Activity(activityJson.getString("slug"),activityJson.getString("start_date"),activityJson.getString("end_date"),activityJson.getString("get_type"));
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        sectionAdapter.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("EROOOOOOOOOOOOOOOOOR","DEU RUIM");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(activityRequest);
+
+
+
 
         return rootView;
     }
