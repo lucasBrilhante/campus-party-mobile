3ba37f893f2df328417bb843bc39064d3ff61b1e
Igor Ribeiro Barbosa Duarte <igor.ribeiro.duarte@gmail.com>
2018-02-03 16:42:07 -0200

    Add geolocalization

--- app/src/main/java/view/activities/MyLocationDemoActivity.java
+++ app/src/main/java/view/activities/MyLocationDemoActivity.java
@@ -1,14 +1,19 @@
 package view.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import cpbr11.campuseromobile.R;
 
@@ -22,7 +27,7 @@ public class MyLocationDemoActivity extends FragmentActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.my_location_demo);
+        setContentView(R.layout.activity_my_location_demo);
 
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
@@ -35,9 +40,25 @@ public class MyLocationDemoActivity extends FragmentActivity
         // TODO: Before enabling the My Location layer, you must request
         // location permission from the user. This sample does not include
         // a request for location permission.
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this, new String[] {
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION },
+                            0);
+        }
         mMap.setMyLocationEnabled(true);
         mMap.setOnMyLocationButtonClickListener(this);
         mMap.setOnMyLocationClickListener(this);
+
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-23.517369f, -46.635706f)).title("Palco Creativity"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(-23.517241f, -46.637225f)).title("Palco Games"));
     }
 
     @Override
